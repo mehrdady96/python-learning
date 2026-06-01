1685b10a0f71eaedec8951a87832b6b8fca8444e
mehrdady96 <mehrdady96@gmail.com>
2026-06-01 13:21:04 +0330

    Update lucki_number.py

--- lucki_number.py
+++ lucki_number.py
@@ -4,14 +4,19 @@ Created on Sun May 31 01:28:51 2026
 
 @author: mad man
 """
-from random import randint
+from random import randint         
 b = int(input("Enter your guess between 1 to 6: "))
 while True:
     a = randint(1,6)
-    if a == b:
+    if b == a : 
         print(f"YOU WON!!! It was {a}\nKeep Going!!!")
-        break
-    else:
+        c = input("Press Y to continue or Q to exit: ").strip().lower()
+        if c == 'y':
+            b = int(input("Enter your guess between 1 to 6: "))
+            continue
+        if c == 'q':
+            break                   
+    else:        
         print(f"YOU LOST!!! It was {a}")
-        b = int(input("Try again: "))
+        d = int(input("Try again: "))
     
