e491494b5e17f5d13c86fe47029429a9f5acd11f
MadMan <mehrdady96@gmail.com>
2026-06-08 12:59:38 +0330

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,24 +1,30 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun  4 12:18:14 2026
+Created on Mon Jun  8 11:21:15 2026
 
 @author: mad man
 """
+
+
 def is_even(n):
-    return (n % 2 == 0)
-
-def even(nums):
-    for n in nums :
-        if is_even(n):
-            return True
-    return False
+ 
+    return n % 2 == 0
 
 
+def get_odds(nums):
+    odds = []
+    count = 0
+    
+    for num in nums:
+        if not is_even(num):
+            odds.append(num)
+            count += 1
+            
+    return  count, odds
 
-my_numbers =  [1,2,3,4,5,6,7,8,9]
-
-print(even(my_numbers))
 
 
-
+numbers = list(range(10))
+count , odds = get_odds(numbers)
+print (count,odds , sep = ' , ')
 
