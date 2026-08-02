61a6567c529d98e6842395b5db8e0799b1e30bc0
MadMan <mehrdady96@gmail.com>
2026-08-02 17:39:20 +0330

    Update lucki_number_2.py

--- lucki_number_2.py
+++ lucki_number_2.py
@@ -3,70 +3,89 @@ created on Friday june 3:18 19/06/2026
 @athur : MadMAn
 """
 def welcome():
+    '''
+    showing welcome messages for the firts time
+    '''
     print("Welcome to this funny game!!!")
     print("Machine picks a number between 1 to 9 and you have to guest it!!!")
 
 def finish():
+    '''
+   asking user for finish or new game
+   '''
     print("Good game!!!")
     answer = input("Do you want to try again?(Y/N)??? ")
-    if answer.upper() == "Y":
-        return True
-    else:
-        return False
+    return answer.upper() == "Y"
 
 
-def win (guess,computer_number):
-    return guess == computer_number
+
+def win (guess,secret_number):
+    '''
+    check if user has won
+    '''
+    return guess == secret_number
 
 
-def check(adam, computer):
-    if adam == computer:
+def check(guess, computer):
+    '''
+    checking user number with random nuber
+    '''
+    if guess == computer:
         return 'Dorost Gofti!!!'
-    if adam > computer:
+    if guess > computer:
         return 'Payin Taaaar!!!!'
-    if adam < computer:
+    if guess < computer:
         return 'Balataaaaaar!!!'
+    return None
 
 def get():
+    '''
+    recieving user number
+    '''
     return int(input("Enter youre guess here: "))
-   
 
-def win (human, computer_number):
-    return human == computer_number
 
-def records():
-    global the_best
-    if the_best == count and count== 0:
-        return "UNBEATBLE!!!"
-    if the_best == 0 :  
+def records(count, the_best):
+    '''
+    register user recods
+    '''
+    if count == 0 and the_best is None:
+        return "UNBEATBLE!!!" , the_best
+    if the_best is None:
         the_best = count
         if count == 1:
-            return(f"The very first record. It took you 1 time ")    
+            message = "The very first record. It took you 1 time "
+            return message, the_best
         if count > 1:
-            return(f"The very first record. It took you {count} times ")  
-    if count < the_best: 
-        the_old_record = the_best
+            message = f"The very first record. It took you {count} times "
+            return message, the_best
+
+    if count < the_best:
+        message = f"The last record was {the_best}, Now is {count}"
         the_best = count
-        return(f"The last record was {the_old_record}, Now is {count}")
+        return message , the_best
     if count == 0 and the_best > 0:
-        return(f"The last record was {the_old_record}, and you guested with zero effort!!!")
-    else:
-        return(f"It took you {count} times. The record is {the_best}")
+        message = f"The last record was {the_best}, and you guested with zero efforts!!!"
+        return message , the_best
+
+    message = f"It took you {count} times. The record is {the_best}"
+    return message , the_best
+
+
 welcome()
+the_best = None
 continue_playing = True
-
-
-the_best = 0
-while (continue_playing):
-        user = 0
-        count = 0
-        import random 
-        computer_number = random.randint(1,9)
-        while (not win(user,computer_number)):
-            user = get()
-            print(check(user,computer_number))
-            if user != computer_number:
-                count += 1
-            if user == computer_number:
-                print(records())
-                continue_playing = finish()
+while continue_playing:
+    user = 0
+    count = 0
+    import random
+    computer_number = random.randint(1,9)
+    while not win(user,computer_number):
+        user = get()
+        print(check(user,computer_number))
+        if user != computer_number:
+            count += 1
+        if user == computer_number:
+            message , the_best = records(count, the_best)
+            print(message)
+            continue_playing = finish()
